eab287dc0a8967d5a437563a0e9a9f390a8b9b65
Juan Aracena <juanjo.aracenasantelises@gmail.com>
2024-11-06 18:48:04 -0500

    Setup postgresql database

--- csrw/settings.py
+++ csrw/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'csrw.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'csrwdb',
+        'USER': 'postgres',
+        'PASSWORD': 'cArSafety#12',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
