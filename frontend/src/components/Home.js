0e8d115c1a05946cb64a6be621d7855cf5aa66ff
Juan Aracena <juanjo.aracenasantelises@gmail.com>
2024-11-18 21:12:17 -0500

    Added search UI

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,6 +5,33 @@ import {useState, useEffect} from "react";
 
 function Home() {
     
+    //Get all makes from NHTSA API
+    const [makeData, setMakeData] = useState([])
+
+    useEffect(() => {
+        fetch(
+            "https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=json"
+        )
+        .then((response) => response.json())
+        .then(setMakeData);
+    }, []);
+
+    // var makeJSONData = JSON.parse(makeData)
+    makeData["Results"].forEach((x, y) => {
+        console.log(x["Make_Name"])
+    })
+
+
+    //  var makeNames = makeJSONData.map(key => key.Make_Name)
+    //  console.log(makeNames)
+
+    // if (makeData)
+    //     return (
+    //         <pre>{JSON.stringify(makeData, null, 2)}</pre>    
+    //     );
+
+    
+
     //Get data from NHTSA API
     // const [nhtsaData, setNhtsaData] = useState([])
 
@@ -15,19 +42,18 @@ function Home() {
     // }, [])
     
 
-
     //Get data from postgresql db
-    const [details, setDetails] = useState([])
+    // const [details, setDetails] = useState([])
 
-    useEffect(() => {
-        axios.get('http://localhost:8000/')
-            .then(response => {
-                setDetails(response.data)
-            })
-            .catch(error => {
-                console.error(error)
-            });
-    }, [])
+    // useEffect(() => {
+    //     axios.get('http://localhost:8000/')
+    //         .then(response => {
+    //             setDetails(response.data)
+    //         })
+    //         .catch(error => {
+    //             console.error(error)
+    //         });
+    // }, [])
 
 
     // let data;
@@ -46,23 +72,51 @@ function Home() {
     //     }
     // })
     
-    
+
     return (
         <div id="welcome">
-            {/* <h1>Car Safety Rating Website</h1> */}
-            <div id="search">
-                <hr></hr>
-                {details.map((output, id) => (
-                    <div key={id}>
-                        <div>
-                            <h2>{output.make}</h2>
-                            <h3>{output.model}</h3>
-                        </div>
-                    </div>
-                ))}
+            <div id="search_div">
+                <form>
+                    <label for="make">Make</label>
+                    <select name="Make" id="make">
+                        {/* {makeJSONData.map((key) =>(
+                           <option value={key.Make_Name}>{key.Make_Name}</option> 
+                        ))} */}
+                    </select>
+                    <label for="model">Model</label>
+                    <select name="Model" id="model">
+                        <option value="test1">Test1</option>
+                        <option value="test2">Test2</option>
+                        <option value="test3">Test3</option>
+                    </select>
+                    <label for="modelyear">Model Year</label>
+                    <select name="Model Year" id="modelyear">
+                        <option value="test1">Test1</option>
+                        <option value="test2">Test2</option>
+                        <option value="test3">Test3</option>
+                    </select>
+                    <input type="submit" value="Search"></input>
+                </form>
             </div>
         </div>
     )
+    
+    // return (
+    //     <div id="welcome">
+    //         {/* <h1>Car Safety Rating Website</h1> */}
+    //         <div id="search">
+    //             <hr></hr>
+    //             {details.map((output, id) => (
+    //                 <div key={id}>
+    //                     <div>
+    //                         <h2>{output.make}</h2>
+    //                         <h3>{output.model}</h3>
+    //                     </div>
+    //                 </div>
+    //             ))}
+    //         </div>
+    //     </div>
+    // )
 }
 
 
